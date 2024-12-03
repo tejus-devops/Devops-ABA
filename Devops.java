14507b43e33347b8f2f3e2c3f2bdd1415ab7aef5
tejus <thequantumcoder@gmail.com>
2024-12-03 15:11:43 +0530

    Initial Commit

---
+++ Devops.java
@@ -0,0 +1,6 @@
+public class Devops {
+    public static void main(String[] args) {
+        System.out.println("DevOps Jenkins Docker Pipeline. ");
+        System.out.println("Created by -\n1. Tejus S \n2. Shreya H V \n3. Shashank L \n4. Shrujan M Nayak");
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+DevOps Jenkins Docker Pipeline.
+
+Created by -
+
+1. Tejus S
+2. Shreya H V
+3. Shashank L
+4. Shrujan M Nayak
