68a637ee7bea1cbeef8421f5b6333bfdf1c5d845
vvce22cse0181@vvce.ac.in <vvce22cv0023@vvce.ac.in>
2024-12-03 12:54:41 +0530

    Add README.md and Dockerfile

--- Devops.java
+++ Devops.java
@@ -1,6 +1,9 @@
-public class Devops{
+
+public class Devops {
+
     public static void main(String[] args) {
         System.out.println("DevOps Jenkins Docker Pipeline. ");
         System.out.println("Created by -\n1. Tejus S \n2. Shreya H V \n3. Shashank L \n4. Shrujan M Nayak");
     }
+
 }
